f232d7ce6d755f3c6237e50c71c714452c331109
Maria Nivetha Antony Pushparaj <63839599+Maria-Antony@users.noreply.github.com>
2025-06-03 01:21:55 +0000

    summarize change

--- nodes/summarize.py
+++ nodes/summarize.py
@@ -1,6 +1,9 @@
-from langchain.chat_models import ChatOpenAI
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from langchain_community.chat_models import ChatOpenAI
 from langchain.chains.summarize import load_summarize_chain
-from prompts import SUMMARY_PROMPT, TIMELINE_PROMPT
+from ..prompts import SUMMARY_PROMPT, TIMELINE_PROMPT
 import os
 
 llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo", openai_api_key=os.environ.get("OPENAI_API_KEY"))
