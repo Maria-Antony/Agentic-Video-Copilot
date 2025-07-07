17a275497e8b4c0087dee89876658e663a733d64
Maria Nivetha Antony Pushparaj <63839599+Maria-Antony@users.noreply.github.com>
2025-07-07 19:38:47 -0400

    Delete utils.py

--- utils.py
+++
@@ -1,6 +0,0 @@
-def extract_video_id(url_or_id):
-    if "youtube.com" in url_or_id or "youtu.be" in url_or_id:
-        import re
-        match = re.search(r"(?:v=|/)([\w-]{11})", url_or_id)
-        return match.group(1) if match else url_or_id
-    return url_or_id
